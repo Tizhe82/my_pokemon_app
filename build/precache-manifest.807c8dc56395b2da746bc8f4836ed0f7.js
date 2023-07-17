b684691d3c0caba71af8be63b915b8c2020e8ffd
Tizhe82 <tizhepaul@gmail.com>
2023-07-17 15:27:19 +0100

    updated dependencies

---
+++ build/precache-manifest.807c8dc56395b2da746bc8f4836ed0f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e08c7e73e4f4c994605",
+    "url": "/static/css/main.b8bf3b57.chunk.css"
+  },
+  {
+    "revision": "6e08c7e73e4f4c994605",
+    "url": "/static/js/main.6e08c7e7.chunk.js"
+  },
+  {
+    "revision": "92192e9e6e97496ce31f",
+    "url": "/static/css/1.de8dd185.chunk.css"
+  },
+  {
+    "revision": "92192e9e6e97496ce31f",
+    "url": "/static/js/1.92192e9e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f4785f3d01a587b70124cd8e34096186",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactDex/precache-manifest.fb05e23a75918336707c384886412ed8.js"
+  "/precache-manifest.807c8dc56395b2da746bc8f4836ed0f7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ReactDex/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
