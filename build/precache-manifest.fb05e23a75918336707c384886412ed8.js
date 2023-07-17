1070e92e37f33130f024628f11e4d648477f851a
Tizhe82 <tizhepaul@gmail.com>
2023-07-17 14:29:15 +0100

    build

---
+++ build/precache-manifest.fb05e23a75918336707c384886412ed8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca0081696b3b726b7e14",
+    "url": "/ReactDex/static/css/main.b8bf3b57.chunk.css"
+  },
+  {
+    "revision": "ca0081696b3b726b7e14",
+    "url": "/ReactDex/static/js/main.ca008169.chunk.js"
+  },
+  {
+    "revision": "883a022ce4ac4be3b027",
+    "url": "/ReactDex/static/css/1.de8dd185.chunk.css"
+  },
+  {
+    "revision": "883a022ce4ac4be3b027",
+    "url": "/ReactDex/static/js/1.883a022c.chunk.js"
+  },
+  {
+    "revision": "bf8c3a743c6b72ed2dd3",
+    "url": "/ReactDex/static/js/runtime~main.bf8c3a74.js"
+  },
+  {
+    "revision": "621ee90fced9308076172ab508b10330",
+    "url": "/ReactDex/index.html"
+  }
+];
